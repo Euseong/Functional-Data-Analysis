5d9b91702f240c3373a634b3591fabd0cbb36cb6
Eunseong <36146343+Euseong@users.noreply.github.com>
2020-08-21 17:25:42 +0900

    Update pda tables(independent error).R

--- pda tables(independent error).R
+++ pda tables(independent error).R
@@ -1,6 +1,19 @@
 library(doParallel)
 library(xtable)
 library(stringr)
+working.dir <- "C:/Users/user/Desktop/장은성/pda research"
+err <- c(0.1, 1) # used 0.1 or 1
+mu <- c(0, 1) # used 0 or 1
+rho <- c(0.1, 0.4, 0.7) # used +-0.1 or +-
+
+a <- merge(err, mu)
+b <- merge(err, rho)
+colnames(a) <- c("err", "mu")
+colnames(b) <- c("err", "rho")
+cov.params <- merge(a, b, by=err)
+simul.params <- rbind(cbind(a, rho=rep(0, nrow(a))), cov.params, cov.params)
+
+
 nc <- detectCores()
 registerDoParallel(nc-1)
 lambdas <- 10^(-5:3)
@@ -15,36 +28,30 @@ gcv.search <- function(lam, fdata., basis){
   return(c(lam, basis$nbasis, mean(gcv)))
 }
 
-err <- 0.1 # used 0.1 or 1
-mu <- 0    # used 0 or 1
 
-mu <- 1
+## 
+ # data set 1
+data.set.list <- data.set1.list
+data.set.num <- 1
+path <- paste0(working.dir, "/data set", data.set.num, " tables.txt")
+scenario.num <- 1
+start.time <- Sys.time()
+for (data.set.i in data.set.list) {
+  data1.fd <- data.set.i$data1.fd
+  data2.fd <- data.set.i$data2.fd
+  params.i <- simul.params[scenario.num,]
+  colnames(params.i) <- NULL
+  err <- params.i[1]; mu <- params.i[2]; rho <- params.i[3]
+  # repeat 28 scenarios (Fourier basis derivative)
 
-err <- 1
-mu <- 0
+  gcv.result1 <- as.data.frame(t(sapply(lambdas, gcv.search, fdata.=data1.fd, basis=bs.basis)))
+  colnames(gcv.result1) <- c("lambda", "n.basis", "gcv")
+  gcv.lambda1 <- gcv.result1$lambda[which.min(gcv.result1$gcv)]
+  gcv.result2 <- as.data.frame(t(sapply(lambdas, gcv.search, fdata.=data2.fd, basis=bs.basis)))
+  colnames(gcv.result2) <- c("lambda", "n.basis", "gcv")
+  gcv.lambda2 <- gcv.result2$lambda[which.min(gcv.result2$gcv)]
+  gcv.mat <- rbind(gcv.mat, c(gcv.lambda1, gcv.lambda2))
 
-mu <- 1#
-## data set 4
-{set.seed(700)
-  data1 <- simul.data(list(cos1, sin1), 100, mu, error=err)
-  data1.fd <- fdata(data1, argvals=x)
-  data2 <- simul.data(list(cos1.05, sin1.05), 100, mu, error=err)
-  data2.fd <- fdata(data2, argvals=x)
-  index <- sample(200, 200)
-  data.set4 <- fdata(rbind(data1, data2)[index,], argvals=x)
-  label4 <- as.factor(c(rep("1", 100), rep("2", 100))[index])}
-
-# repeat this for 4 scenarios (Fourier basis derivative)
-{
-gcv.result1 <- as.data.frame(t(sapply(lambdas, gcv.search, fdata.=data1.fd, basis=bs.basis)))
-colnames(gcv.result1) <- c("lambda", "n.basis", "gcv")
-gcv.lambda1 <- gcv.result1$lambda[which.min(gcv.result1$gcv)]
-gcv.result2 <- as.data.frame(t(sapply(lambdas, gcv.search, fdata.=data2.fd, basis=bs.basis)))
-colnames(gcv.result2) <- c("lambda", "n.basis", "gcv")
-gcv.lambda2 <- gcv.result2$lambda[which.min(gcv.result2$gcv)]
-gcv.mat <- rbind(gcv.mat, c(gcv.lambda1, gcv.lambda2))
-}
-{
   data1.smooth <- smooth.fdata(data1.fd, basis=bs.basis, lambda=gcv.lambda1)
   data2.smooth <- smooth.fdata(data2.fd, basis=bs.basis, lambda=gcv.lambda2)
   set.seed(100)
@@ -63,8 +70,7 @@ gcv.mat <- rbind(gcv.mat, c(gcv.lambda1, gcv.lambda2))
   
   set.bsm.result0.rt <- rbind(rbind(set.sm.RSQ.error, set.sm.pda.error), set.sm.fpc.error$test.errors)#, max.fpc=c("-", "-", 1:5), stringsAsFactors=F)
   set.bsm.result0.bayes <- rbind(rbind(set.sm.RSQ.bayes.error, set.sm.pda.bayes.error), set.sm.fpc.error$bayes.error)
-}
-{
+
   gcv.result1 <- data.frame(t(sapply(lambdas, gcv.search, fdata.=data1.fd, basis=f.basis)))
   colnames(gcv.result1) <- c("lambda", "n.basis", "gcv")
   gcv.lambda1 <- gcv.result1$lambda[which.min(gcv.result1$gcv)]
@@ -72,8 +78,7 @@ gcv.mat <- rbind(gcv.mat, c(gcv.lambda1, gcv.lambda2))
   colnames(gcv.result2) <- c("lambda", "n.basis", "gcv")
   gcv.lambda2 <- gcv.result2$lambda[which.min(gcv.result2$gcv)]
   gcv.mat <- rbind(gcv.mat, c(gcv.lambda1, gcv.lambda2))
-}
-{
+
   data1.smooth <- smooth.fdata(data1.fd, basis=f.basis, lambda=gcv.lambda1)
   data2.smooth <- smooth.fdata(data2.fd, basis=f.basis, lambda=gcv.lambda2)
   set.seed(100)
@@ -96,14 +101,22 @@ gcv.mat <- rbind(gcv.mat, c(gcv.lambda1, gcv.lambda2))
   set.sm.result0.rt <- cbind(paste(round(set.bsm.result0.rt[,1], 2), " (", round(set.bsm.result0.rt[,2], 2), ")", sep=""), #set.bsm.result0.rt[,3],
                              paste(round(set.fsm.result0.rt[,1], 2), " (", round(set.fsm.result0.rt[,2], 2), ")", sep=""))#, set.fsm.result0.rt[,3])
   set.sm.result0.bayes <- cbind(paste(round(set.bsm.result0.bayes[,1], 2), " (", round(set.bsm.result0.bayes[,2], 2), ")", sep=""),
-                             paste(round(set.fsm.result0.bayes[,1], 2), " (", round(set.fsm.result0.bayes[,2], 2), ")", sep=""))
+                                paste(round(set.fsm.result0.bayes[,1], 2), " (", round(set.fsm.result0.bayes[,2], 2), ")", sep=""))
   
   row.names(set.sm.result0.rt) <- c("PDA(RSQ)", "& & PDA(scores)", paste(rep("& & FPCA (num of PCs=", 5), as.character(1:5), ")", sep=""))
   row.names(set.sm.result0.bayes) <- c("PDA(RSQ)", "& & PDA(scores)", paste(rep("& & FPCA (num of PCs=", 5), as.character(1:5), ")", sep=""))
-  xtable(set.sm.result0.rt, caption=str_interp("data set ${} test error (mu=${mu}, error=${err})"))
-  xtable(set.sm.result0.bayes, caption=str_interp("data set ${} bayes error (mu=${mu}, error=${err})"))
+  test.error <- xtable(set.sm.result0.rt,
+                       caption=str_interp("data set ${data.set.num} ${scenario.num}th test error (mu=${mu}, error=${err}, rho=${rho})"))
+  bayes.error <- xtable(set.sm.result0.bayes,
+                        caption=str_interp("data set ${data.set.num} ${scenario.num}th bayes error (mu=${mu}, error=${err}, rho=${rho})"))
+  
+  is.append <- scenario.num > 1
+  print(test.error, file = path, compress = FALSE, append=is.append)
+  print(bayes.error, file = path, compress = FALSE, append=T)
+  scenario.num <- scenario.num + 1
+  print(paste0(scenario.num, "th scenario over"))
 }
-
+paste("start :", start.time, "end :", Sys.time())
 
 
 
@@ -199,5 +212,4 @@ mu <- 1#
 }
 # write.csv(gcv.mat, "lambda.csv")
 
-print(a, file = "a.txt", compress = FALSE, append=T)
-print(xtable(set.sm.result0.bayes), file = "a.txt", compress = FALSE, append=T)
+
